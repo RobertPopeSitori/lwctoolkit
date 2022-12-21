e3baf7ee4b6e7b079ff917cefabd056ab7add17f
Robert Pope <robert.pope@onlinelearning-c.org>
2022-12-21 14:25:05 -0600

    Fixed a Typo

--- lwcToolKit/force-app/main/default/lwc/memoryGame/memoryGame.js
+++ lwcToolKit/force-app/main/default/lwc/memoryGame/memoryGame.js
@@ -3,17 +3,16 @@ import {loadStyle} from 'lightning/platformResourceLoader'
 import fontawesome from '@salesforce/resourceUrl/fontawesome'
 export default class MemoryGame extends LightningElement {
 	isLibLoaded = false
-	renderCallback(){
-		if(this.isLibLoaded){
-			return
-		} else {
-			loadStyle(this,fontawesome+'/fontawesome/css/font-awesome.min.css').then(()=>{
-				console.log("Fontawesome Loaded successfully")
-			}).catch(error=>{
-				console.error(error)
-			})
-			this.isLibLoaded = true
-		}
-		
-	}
+	renderedCallback(){
+        if(this.isLibLoaded){
+            return
+        } else {
+            loadStyle(this,fontawesome+'/fontawesome/css/font-awesome.min.css').then(()=>{
+                console.log("loaded successfully")
+            }).catch(error=>{
+                console.error(error)
+            })
+            this.isLibLoaded = true
+        }
+    }
 }
