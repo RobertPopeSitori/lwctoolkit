06af562b29036bec2f52d541f06732e5cf332704
Robert Pope <robert.pope@onlinelearning-c.org>
2022-12-21 14:15:55 -0600

    first draft

--- lwcToolKit/force-app/main/default/lwc/memoryGame/memoryGame.js
+++ lwcToolKit/force-app/main/default/lwc/memoryGame/memoryGame.js
@@ -1,3 +1,19 @@
 import { LightningElement } from 'lwc';
-
-export default class MemoryGame extends LightningElement {}
+import {loadStyle} from 'lightning/platformResourceLoader'
+import fontawesome from '@salesforce/resourceUrl/fontawesome'
+export default class MemoryGame extends LightningElement {
+	isLibLoaded = false
+	renderCallback(){
+		if(this.isLibLoaded){
+			return
+		} else {
+			loadStyle(this,fontawesome+'/fontawesome/css/font-awesome.min.css').then(()=>{
+				console.log("Fontawesome Loaded successfully")
+			}).catch(error=>{
+				console.error(error)
+			})
+			this.isLibLoaded = true
+		}
+		
+	}
+}
